2b01abcd48d6f84659bd0ad2e7209d74c21e3999
Anand.Pandey <anandkp@gmail.com>
2019-02-04 17:16:38 -0500

    Updates

---
+++ precache-manifest.560f55b9a2f2e8f3224cbbeb81f29d6d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/json-ui-pnc/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "a98963676bfeef44e6b3",
+    "url": "/json-ui-pnc/static/css/main.210b4655.chunk.css"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/json-ui-pnc/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "a747748261035fd92367",
+    "url": "/json-ui-pnc/static/js/1.a7477482.chunk.js"
+  },
+  {
+    "revision": "3c7a083a78be12275781",
+    "url": "/json-ui-pnc/static/js/runtime~main.3c7a083a.js"
+  },
+  {
+    "revision": "a98963676bfeef44e6b3",
+    "url": "/json-ui-pnc/static/js/main.a9896367.chunk.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/json-ui-pnc/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/json-ui-pnc/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/json-ui-pnc/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "a747748261035fd92367",
+    "url": "/json-ui-pnc/static/css/1.1a46cd1a.chunk.css"
+  },
+  {
+    "revision": "df08af5241511b6666e7158abf2f4dcf",
+    "url": "/json-ui-pnc/index.html"
+  }
+];
